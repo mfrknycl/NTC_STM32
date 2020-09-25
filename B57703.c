23a82c65c6f7f3a461bac4d3fee55b43247838a7
Furkan <mfurkanycl@gmail.com>
2020-09-25 15:43:30 +0300

    Library updated
    8 ADC -> 12 ADC

--- B57703.c
+++ B57703.c
@@ -1,5 +1,23 @@
 #include "B57703.h"
 
+/*
+ *
+ *		          Linear interpolation equation 
+ *
+ *			        		y0(x1-x) + y1(x - x0)		
+ *			        y = ----------------------
+ *			        					 x1-x0
+ *
+ *
+ *			y0 = ntcTemp[i].y      x0 = ntcTemp[i].x
+ *			y1 = ntcTemp[i+1].y    x1 = ntcTemp[i+1].x
+ *			
+ *	
+ * 
+ */
+
+
+
 /**
 	* @brief Find the range of temperature from lookup table.
 						This function uses liner interpolation method.
@@ -8,23 +26,20 @@
 	* @param size is the size of the *table
 	* @retval it returns the temperature after it's calculated
 	*/
-double findTemp(const ntcStruct_t *table, double adcVal, int size){
+float findTemp(const ntcStruct_t *table, float adcVal, int size){
 		
-	//y = (y0*(x1-x)+y1*(x-x0))/(x1-x0)
-	//y0 = ntcTemp[i].y
-	//y1 = ntcTemp[i+1].y
-	//x0 = ntcTemp[i].x
-	//x1 = ntcTemp[i+1].x
+	float y_ans;
 	
-	double y_ans;
-	
-	for(int i = 0; i < size-1; i++){
+	for(int i = 0; i < size; i++){
+		
+		if(adcVal > ntcTemp[0].x){break;}	
+		
 		if(ntcTemp[i].x <= adcVal && adcVal >= ntcTemp[i+1].x){
 			y_ans = (ntcTemp[i].y*(ntcTemp[i+1].x-adcVal)+ntcTemp[i+1].y*(adcVal-ntcTemp[i].x))/(ntcTemp[i+1].x-ntcTemp[i].x);
 			return y_ans;
 		}
 	}
-	return 0;
+	return -1;
 }
 
 
